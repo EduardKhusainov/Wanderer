2041fee429f1568111ce6f93d92de7931efb8004
Artiom Sudin <artem.sudin@gmail.com>
2024-01-29 10:52:46 +0300

    Fixed merge conflict

--- Assets/LoadNext.cs
+++ Assets/LoadNext.cs
@@ -1,38 +1,40 @@
-using System.Threading.Tasks;
 using UnityEngine;
 using Wanderer.NSUI.Overlay;
 using Wanderer.NSUI.Screen;
 
 namespace Wanderer
 {
-   [SerializeField] GameObject[] maps;
-   [SerializeField] PlayerController playerController;
-   PlayerResetPos playerResetPos;
-   int currentIndex;
-   bool isMove;
-    
-    private void Start() 
+    public class LoadNext : MonoBehaviour
     {
-        playerController = FindObjectOfType<PlayerController>();    
-        playerResetPos =  FindObjectOfType<PlayerResetPos>();
-        currentIndex = 0;
-    }
-    private void OnTriggerEnter(Collider other) 
-    {
-        if(other.gameObject.CompareTag("Player"))
-        {
-            ShowLevelChanger();
-            maps[currentIndex].SetActive(false);
-            currentIndex++;
-            maps[currentIndex].SetActive(true);
-            playerController.isTeleported = true;
-            playerController.transform.position = new Vector3(0,0,-15f);
-            playerResetPos.isMove = true;
-            this.gameObject.SetActive(false);
-        } 
-    }
+        [SerializeField] GameObject[] maps;
+        [SerializeField] PlayerController playerController;
+        PlayerResetPos playerResetPos;
+        int currentIndex;
+        bool isMove;
 
-    private async void ShowLevelChanger()
+        private void Start()
+        {
+            playerController = FindObjectOfType<PlayerController>();
+            playerResetPos = FindObjectOfType<PlayerResetPos>();
+            currentIndex = 0;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                ShowLevelChanger();
+                maps[currentIndex].SetActive(false);
+                currentIndex++;
+                maps[currentIndex].SetActive(true);
+                playerController.isTeleported = true;
+                playerController.transform.position = new Vector3(0, 0, -15f);
+                playerResetPos.isMove = true;
+                this.gameObject.SetActive(false);
+            }
+        }
+
+        private async void ShowLevelChanger()
         {
             var canvas = CanvasManager.Instance.SetCanvases[2].GetComponent<LevelChangeOverlay>();
             var fadeDelay = 1f;
